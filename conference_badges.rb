ac2fde863c02d8018217a8d2a97dc6387f51ec79
Kevin <kevinlai76@gmail.com>
2019-05-23 18:04:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each { |badge| badges << badge_maker(badge) }
+  return badges
+end 
+
+def assign_rooms(array)
+end 
